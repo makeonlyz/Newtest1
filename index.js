570bcac0a4209ab50881206de52b5bb9a24b9f2c
makeonlyz <steamkeyshop@gmail.com>
2023-06-07 00:35:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-const posts = require('./posts_new.json');
-
 module.exports = async (req, res) => {
   const { PostID } = req.query;
 
-  // Find the post with the matching PostID
-  const post = posts.find((p) => p.postid === parseInt(PostID));
-
-  if (post) {
-    // Post found, send the response
-    res.status(200).send(`
-      <h1>${post.title}</h1>
-      <img src="${post.metaValue}" alt="Post Image" />
-    `);
-  } else {
-    // Post not found
-    res.status(404).send('Post not found');
-  }
+  res.status(200).send(`
+    <h1>Post ID: ${PostID}</h1>
+  `);
 };
